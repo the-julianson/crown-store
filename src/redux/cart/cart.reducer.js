ac9a9a6f4b8e99b290d5349f43bee9ae32187ee5
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-11 10:37:10 -0300

    added CLEAR_ITEM_FROM_CART action, updated the cart reducer and modify the checkout-item comp to fire the cart reducer for removing a complete item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -21,6 +21,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         //now we pass the utility function
         cartItems: addItemToCart(state.cartItems, action.payload)
       }
+    case cartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+      }
     default:
       return state;
   }
